8e14d3b883e32c76c66d00ed35fb892be14ea3db
mmilitaryminds-creator <mmilitaryminds@gmail.com>
2026-09-02 20:19:57 +0100

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -50,7 +50,7 @@ module.exports = async (req, res) => {
         const groq = new Groq({ apiKey: groqApiKey });
         const completion = await groq.chat.completions.create({
           messages: [{ role: "user", content: fullContext }],
-          model: "llama-3.3-70b-versatile"
+          model: "llama-3.1-8b-instant"
         });
         const response = completion.choices[0].message.content;
 
